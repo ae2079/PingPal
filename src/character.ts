6dd138f9af9947d259b60b63d15974f27185549d
Alireza Sharifpour <ascode94@gmail.com>
2025-04-14 17:26:11 +0330

    Update PingPal character configuration: add Telegram client, include API key in secrets, and specify model version for enhanced functionality.

--- src/character.ts
+++ src/character.ts
@@ -4,16 +4,19 @@ import {
   defaultCharacter,
   ModelProviderName,
 } from "@elizaos/core";
+
 import PingPal from "../characters/pingpal.character.json" with { type: "json" };
 
 export const character: Character = {
   ...defaultCharacter,
   name: PingPal.name,
-  plugins: [],
-  clients: [],
+  clients: [Clients.TELEGRAM],
   modelProvider: ModelProviderName.OPENAI,
   settings: {
-    secrets: {},
+    secrets: {
+      key: process.env.TELEGRAM_API_KEY,
+    },
+    model: "gpt-4o-mini",
     voice: {
       model: "en_US-hfc_female-medium",
     },
@@ -22,7 +25,15 @@ export const character: Character = {
   bio: PingPal.bio,
   lore: PingPal.lore,
   messageExamples: PingPal.messageExamples,
+  postExamples: PingPal.postExamples,
   adjectives: PingPal.adjectives,
   topics: PingPal.topics,
   style: PingPal.style,
+
+  // allowDirectMessages: PingPal.allowDirectMessages,
+  // shouldOnlyJoinInAllowedGroups: PingPal.shouldOnlyJoinInAllowedGroups,
+  // allowedGroupIds: PingPal.allowedGroupIds,
+  // messageTrackingLimit: PingPal.messageTrackingLimit,
+  // templates: PingPal.templates,
+  // secrets: PingPal.secrets,
 };
